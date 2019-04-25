74792278a6d597296ef548a3759893099ed076e4
FranBelt <franbeltranbach@gmail.com>
2019-04-25 13:57:44 +0200

    Limpiando codigo...

--- prueba.py
+++ prueba.py
@@ -3,12 +3,16 @@ import requests
 from bs4 import BeautifulSoup   # Scraping HTML
 import re
 import datetime
+import os
+import inspect
+
+PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 # PATH
 #path = "/home/fran/Escritorio/Proyecto2/salida.txt"
 #file = open(path, "w+")
 
-def date1(dd, mm, yyyy):                            # yyyy-MM-dd
+def date1(dd, mm, yyyy): # yyyy-MM-dd
     if dd < 10 and mm < 10:
         return str("%i-0%i-0%i" % (yyyy, mm, dd))
 
@@ -21,7 +25,7 @@ def date1(dd, mm, yyyy):                            # yyyy-MM-dd
     if dd > 10 and mm >= 10:
         return str("%i-%i-%i" % (yyyy, mm, dd))
 
-def date2(dd, mm, yyyy):                            # dd-MM-yyyy
+def date2(dd, mm, yyyy): # dd-MM-yyyy
     if dd < 10 and mm < 10:
         return str("0%i-0%i-%i" % (dd, mm, yyyy))
 
@@ -37,8 +41,14 @@ def date2(dd, mm, yyyy):                            # dd-MM-yyyy
 def data_url(fecha1, fecha2):
     return str("http://www.movistarplus.es/programacion-tv/" + fecha1 + "/?v=json&verticalScroll=true&isMobile=true&date=Tue+Dec+06+2016+00%3A00%3A00+GMT%2B0000+(Coordinated+Universal+Time)&calculada=Tue+Dec+06+2016+00%3A00%3A00+GMT%2B0000+(Coordinated+Universal+Time)&fecha=" + fecha2)
 
-# url with .json
-#url = "http://www.movistarplus.es/programacion-tv/" + fecha1 + "/?v=json&verticalScroll=true&isMobile=true&date=Tue+Dec+06+2016+00%3A00%3A00+GMT%2B0000+(Coordinated+Universal+Time)&calculada=Tue+Dec+06+2016+00%3A00%3A00+GMT%2B0000+(Coordinated+Universal+Time)&fecha=" + fecha2
+def load_epg(PATH, name):
+    with open("%s/src/%s" % (PATH, name)) as DATA_FILE:
+        temp = json.load(DATA_FILE)
+        DATA = json.loads(json.dumps(temp["data"]))
+
+        return DATA
+
+#print(load_epg(PATH, "descarga.json"))
 
 def download_epg(url):
     DATA_FILE = requests.get(url).json()
@@ -51,9 +61,17 @@ def download_epg(url):
 # Parsing the first category
 #DATOS = json.loads(json.dumps(DATA_FILE["data"]))
 
-# Get all the Channels-ID
-#for x in DATOS:
-#    print(x)
+def get_all_channels_code(DATA):
+    size = len(DATA)
+    vector = []
+
+    for x in DATA:
+        vector.append(x)
+
+    return vector
+
+
+#get_all_channels_code(load_epg(PATH, "descarga.json"))
 
 def get_epg_channel(channel_code, DATA):
     CHANNEL = json.loads(json.dumps(DATA[channel_code]))
@@ -61,7 +79,7 @@ def get_epg_channel(channel_code, DATA):
 
     return CHANNEL_SHOWS
 
-#print(get_epg_channel("MV1-CODE", download_epg(data_url(date1(25,4,2019),date2(25,4,2019)))))
+#print(get_epg_channel("MV1-CODE", load_epg(PATH, "descarga.json")))
 
 # Parsing into the Channel
 #CANAL = json.loads(json.dumps(DATOS["MV1-CODE"]))
@@ -79,7 +97,10 @@ def collecting_shows(epg_channel):
             else:
                 print("TEMPORADA: %s" % show['TEMPORADA'][2:4])
 
-            print("EPISODIO: %s" % segmento[1])
+            if len(segmento) > 2:
+                print("EPISODIO: %s" % segmento[1])
+            else:
+                print("EPISODIO: None")
 
         else:
             print("TITULO: %s" % show['TITULO'])
@@ -99,10 +120,14 @@ def collecting_shows(epg_channel):
         soup = BeautifulSoup(page.text, 'html.parser')
 
         # Scraping Sinopsis from URL
-        sinopsis = soup.find('div', {"class": "text show-more-height"})
-        sinopsis_content = sinopsis.find('p')
-        clear_sinopsis = str(sinopsis_content).replace('<p>', '')
-        clear_sinopsis = clear_sinopsis.replace('</p>', '')
+        try:
+            sinopsis = soup.find('div', {"class": "text show-more-height"})
+            sinopsis_content = sinopsis.find('p')
+            clear_sinopsis = str(sinopsis_content).replace('<p>', '')
+            clear_sinopsis = clear_sinopsis.replace('</p>', '')
+        except AttributeError:
+            clear_sinopsis = ''
+
         print("SINOPSIS: %s" % clear_sinopsis)
 
         # Scraping Languages and Subtitles
@@ -112,15 +137,18 @@ def collecting_shows(epg_channel):
         print(subtitles)
 
         # Scraping Quality
-        content = soup.find('ul', {'class': 'list-info-movie'})
-        quality = content.findAll('li')
+        try:
+            content = soup.find('ul', {'class': 'list-info-movie'})
+            quality = content.findAll('li')
         
-        if len(quality) == 1:
-            try:
+            if len(quality) == 1:
                 quality_clear = re.search('alt="(.+?)" src=', str(quality)).group(1)
-            except AttributeError:
-                quality_clear = ''
+                print(quality_clear)
+
+        except AttributeError:
+            quality_clear = ''
             print(quality_clear)
+
         
         # Scraping Moral
         content = soup.find('div', {'class': 'moral'})
@@ -133,10 +161,7 @@ def collecting_shows(epg_channel):
         print("Calificación: %s" % moral)
         print("")
 
-for x in range(25, 27):
-    print("EPG DAY %i:" % x)
-    collecting_shows(get_epg_channel("FOXGE-CODE", download_epg(data_url(date1(x,4,2019),date2(x,4,2019)))))
-
-    print("Grabbing finished succesfully\n")'''
-
-#file.close()
+for x in (get_all_channels_code(load_epg(PATH, "descarga.json"))):
+    print("CANAL: %s" % x)
+    collecting_shows(get_epg_channel(x, load_epg(PATH, "descarga.json")))
+    print("Grabbing finished succesfully\n")
